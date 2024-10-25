5febc333b4a971da56f544c5bea948eb5020f146
ANMOLSCRIPT <anmolgarg1268@gmail.com>
2024-10-25 21:18:22 +0530

    updated MainActivity file

--- app/src/main/java/com/example/passwordmanager/MainActivity.kt
+++ app/src/main/java/com/example/passwordmanager/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.passwordmanager
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.passwordmanager.database.PasswordDatabase
+import com.example.passwordmanager.repository.PasswordRepository
+import com.example.passwordmanager.viewodel.PasswordViewModel
+import com.example.passwordmanager.viewodel.PasswordViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var passwordViewModel: PasswordViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel(){
+        val passwordRepository = PasswordRepository(PasswordDatabase(this))
+        val viewModelProviderFactory = PasswordViewModelFactory(application, passwordRepository)
+        passwordViewModel = ViewModelProvider(this, viewModelProviderFactory)[PasswordViewModel::class.java]
     }
 }
